b00b3e32b677f34475ae7abc5673895dfebca17e
Bernd Riegg <bernd@riegg.de>
2011-11-07 13:24:40 +0100

    second commit

--- src/ph/balikbayan/dev/client/Balikbayan.java
+++ src/ph/balikbayan/dev/client/Balikbayan.java
@@ -23,7 +23,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class Balikbayan implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
+	 * returns an error. ändern.
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
